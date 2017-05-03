a51276bb7cd918df2bbcbe8a5366f34d0b1be486
Asha <Asha>
2017-05-03 16:06:41 -0700

    Updated so that the tests pass for inheritance and functional implementation of filter method.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,17 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
       var productsICanEat = [];
-
+	  var nutFreeProducts = _(products).filter(function (x){return (x.containsNuts == false)});
+	  
       /* solve using filter() & all() / any() */
-
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+	  var productsICanEat = _(nutFreeProducts).filter(function (x){return (x.ingredients.indexOf("mushrooms") == -1});
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
